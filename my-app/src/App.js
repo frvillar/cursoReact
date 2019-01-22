e9db319398a988119613aed87edf53af38766d02
frvillar <frvillar@gmail.com>
2019-01-22 16:14:12 -0300

    my-app

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Aplicação React Número 1</h1>
+      </div>
+      // React.createElement('div',{className: 'App'}, React.createElement('h1',null,'Aplicação React 1'))
+      );
+  }
+}
+
+export default App;
